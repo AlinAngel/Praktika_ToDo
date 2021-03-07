0743136182c2097d5350aa13219bf93662de8ae3
AlinAngel <mr.vip.iamdarkness@mail.ru>
2021-03-08 00:38:30 +0300

    Add search

--- scripts.js
+++ scripts.js
@@ -5,12 +5,13 @@ const addFilmForm = document.querySelector("#add-film-form")
 const allFilmsList = document.querySelector(".all-films-items")
 const finishedFilmsList = document.querySelector(".finished-films-items")
 const likeFilmsList = document.querySelector(".like-films-items")
+const searchForm = document.querySelectorAll('.search-form')
 
 addButton.addEventListener("click", (e) => {
     // e.preventDefault();
     modal.style.display = "block";
 
-    window.addEventListener('click', (e) => {
+    addFilmForm.addEventListener('click', (e) => {
         e.preventDefault()
         if (e.target === modal) {
             modal.style.display = "none"
@@ -42,10 +43,10 @@ const addFilm = (film) => {
 
     const newFilm = `
     <div class="item-film">
-        <p>${film}</p>
-        <div class="all-films-itemImg">
-            <img src="img/eye.png" class="img-films move-btn">
-            <img src="img/trash-bin.png" class="img-films remove-btn">
+        <p class="film-name">${film}</p>
+        <div class="img-item-film">
+            <img src="img/eye.png" class="img-films-L move-btn">
+            <img src="img/trash-bin.png" class="img-films-R remove-btn">
         </div>
     </div>
     `
@@ -55,7 +56,7 @@ const addFilm = (film) => {
 // удаление фильма по нажатию на иконку корзины
 allFilmsList.addEventListener("click", (e) => {
     e.preventDefault();
-    if (e.target.className != "img-films remove-btn") return;
+    if (e.target.className != "img-films-R remove-btn") return;
 
     let removeAllFilm = e.target.closest(".item-film");
     removeAllFilm.remove();
@@ -63,7 +64,7 @@ allFilmsList.addEventListener("click", (e) => {
 
 finishedFilmsList.addEventListener("click", (e) => {
     e.preventDefault();
-    if (e.target.className != "img-films remove-btn") return;
+    if (e.target.className != "img-films-R remove-btn") return;
 
     let removeFinishedFilm = e.target.closest(".item-film");
     removeFinishedFilm.remove();
@@ -72,7 +73,7 @@ finishedFilmsList.addEventListener("click", (e) => {
 
 likeFilmsList.addEventListener("click", (e) => {
     e.preventDefault();
-    if (e.target.className != "img-films remove-btn") return;
+    if (e.target.className != "img-films-R remove-btn") return;
 
     let removeLikeFilm = e.target.closest(".item-film");
     removeLikeFilm.remove();
@@ -82,17 +83,17 @@ likeFilmsList.addEventListener("click", (e) => {
 // перемещение фильма по нажатию на иконку глаза
 allFilmsList.addEventListener("click", (e) => {
     e.preventDefault();
-    if (e.target.className != "img-films move-btn") return;
+    if (e.target.className != "img-films-L move-btn") return;
     let moveFinishedFilm = e.target.closest(".item-film");
 
     finishedFilmsList.insertAdjacentHTML("beforeend", `
     <div class="item-film item-finished-film">
-        <p> ${moveFinishedFilm.textContent
+        <p class="film-name">${moveFinishedFilm.textContent
             // .replace(/\s{2,}/g,' ')
-            .trim()} </p>
-        <div class="all-films-itemImg">
-            <img src="img/heart.png" class="img-films like-btn" >
-            <img src="img/trash-bin.png" class="img-films remove-btn">
+            .trim()}</p>
+        <div class="img-item-film">
+            <img src="img/heart.png" class="img-films-L like-btn" >
+            <img src="img/trash-bin.png" class="img-films-R remove-btn">
         </div>
     </div>`);
 
@@ -102,17 +103,17 @@ allFilmsList.addEventListener("click", (e) => {
 // перемещение фильма по нажатию на иконку сердечка
 finishedFilmsList.addEventListener("click", (e) => {
     e.preventDefault();
-    if (e.target.className != "img-films like-btn") return;
+    if (e.target.className != "img-films-L like-btn") return;
     let moveLikeFilm = e.target.closest(".item-film");
 
     likeFilmsList.insertAdjacentHTML("beforeend", `
     <div class="item-film">
-        <p> ${moveLikeFilm.textContent
+        <p class="film-name">${moveLikeFilm.textContent
             // .replace(/[\n\r]+|[\s]{2,}/g,' ')
-            .trim()} </p>
-        <div class="all-films-itemImg">
-            <img src="img/broken-heart.png" class="img-films unlike-btn">
-            <img src="img/trash-bin.png" class="img-films remove-btn">
+            .trim()}</p>
+        <div class="img-item-film">
+            <img src="img/broken-heart.png" class="img-films-L unlike-btn">
+            <img src="img/trash-bin.png" class="img-films-R remove-btn">
         </div>
     </div>`);
 
@@ -122,19 +123,56 @@ finishedFilmsList.addEventListener("click", (e) => {
 // перемещение фильма по нажатию на иконку разбитого сердечка
 likeFilmsList.addEventListener("click", (e) => {
     e.preventDefault();
-    if (e.target.className != "img-films unlike-btn") return;
+    if (e.target.className != "img-films-L unlike-btn") return;
     let moveUnlikeFilm = e.target.closest(".item-film");
 
     finishedFilmsList.insertAdjacentHTML("beforeend", `
     <div class="item-film item-finished-film">
-        <p> ${moveUnlikeFilm.textContent
+        <p class="film-name">${moveUnlikeFilm.textContent
             // .replace(/\s{2,}/g,' ')
-            .trim()} </p>
-        <div class="all-films-itemImg">
-            <img src="img/heart.png" class="img-films like-btn">
-            <img src="img/trash-bin.png" class="img-films remove-btn">
+            .trim()}</p>
+        <div class="img-item-film">
+            <img src="img/heart.png" class="img-films-L like-btn">
+            <img src="img/trash-bin.png" class="img-films-R remove-btn">
         </div>
     </div>`);
 
     moveUnlikeFilm.remove();
 })
+
+
+// поиск фильма
+function searchGoal(word, listName) {
+    let list;
+
+    switch (listName) {
+        case "searchLike": 
+            list = Array.from(likeFilmsList.children)
+            break;
+        case "searchWatched": 
+            list = Array.from(finishedFilmsList.children)
+            break;
+        default:  
+            list = Array.from(allFilmsList.children)
+            break;
+    }
+
+    list.forEach(film => {
+      const content = film.querySelector(".film-name").innerText.toLowerCase();
+      if (!content.includes(word.toLowerCase())) {
+        film.classList.add("hide");
+      } else {
+        film.classList.remove("hide");
+      }
+    });
+  }
+
+  for (let i = 0; i < searchForm.length; i++) { 
+    searchForm[i].addEventListener("keyup", (event) => {
+        event.preventDefault();
+        const searchWord = searchForm[i][event.target.name].value;
+
+        searchGoal(searchWord, event.target.name);
+    });
+  }
+
